d32316ffd1f4e8272c57f18aefd01a3d41fc8537
Sarah Ganci <sarahganci@gmail.com>
2018-10-22 11:54:46 -0400

    added saved stuff

--- Lab2.java
+++ Lab2.java
@@ -5,6 +5,21 @@ public class Lab2 {
 	public static void main(String[] args) {
 		Charlie charlie= new Charlie();
 
+		charlie.syncMotors();
+		
+		//move towards wall
+		charlie.moveTillSense(.20);
+		
+		//turn sensor
+		charlie.rotateSonic(90, true );
+		
+		//turn robot right
+		
+		//follow wall
+		
+		//turn robot left
+		
+		//move forward distance till goal
 	}
 
 }
